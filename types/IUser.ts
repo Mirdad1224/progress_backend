09717955dad343452697cd63986539d59c9a2b9e
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-10 17:20:46 +0330

    create user model interface

---
+++ types/IUser.ts
@@ -0,0 +1,25 @@
+import { Types } from "mongoose";
+
+export interface IAddresses {
+  state: string;
+  city: string;
+  street: string;
+  postalCode: string;
+}
+
+export default interface IUser {
+  email: string;
+  password?: string;
+  name?: string;
+  image?: string;
+  phone?: string;
+  addresses?: IAddresses[];
+  role: "user" | "admin" | "superAdmin";
+  favorates?: [Types.ObjectId];
+  basket?: {
+    totalAmount: number;
+    totalQTY: number;
+    cartItems: [productId: Types.ObjectId, cartQuantity: number];
+  };
+  refreshToken?: string;
+}
