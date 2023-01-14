23c3ba9277b00ece57b2ccfa76c6fd2e6353c50a
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-14 18:13:44 +0330

    create order controller

---
+++ controllers/order.ts
@@ -0,0 +1,135 @@
+import mongoose from "mongoose";
+import { Request, Response, NextFunction } from "express";
+import errorGenerate from "../utils/errorGenerate";
+import Order from "../models/Order";
+import User from "../models/User";
+import Product from "../models/Product";
+import checkOrder from "../validators/order";
+
+export const getOrders = async (
+  req: Request<{}, {}, {}, { page?: string; limit?: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const pageNumber = parseInt(req.query.page || "1");
+  const nPerPage = parseInt(req.query.limit || "6");
+  try {
+    const orders = await Order.find()
+      .populate("productId")
+      .populate("userId")
+      .sort({ _id: 1 })
+      .skip((pageNumber - 1) * nPerPage)
+      .limit(nPerPage);
+    const totalOrders = await Order.countDocuments();
+
+    if (!orders) {
+      errorGenerate("Orders not found", 404);
+    }
+    res
+      .status(200)
+      .json({ message: "Orders found", data: orders, total: totalOrders });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getOrdersByUserId = async (
+  req: Request<{ uid: string }, {}, {}, { page?: string; limit?: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.uid)) {
+    errorGenerate("Invalid ID", 400);
+  }
+
+  const pageNumber = parseInt(req.query.page || "1");
+  const nPerPage = parseInt(req.query.limit || "6");
+  try {
+    const userWithOrders = await User.findById(req.params.uid)
+      .populate({ path: "orders", populate: { path: "productId" } })
+      .sort({ _id: 1 })
+      .skip((pageNumber - 1) * nPerPage)
+      .limit(nPerPage);
+
+    if (!userWithOrders) {
+      errorGenerate("User has no orders", 404);
+    }
+    res
+      .status(200)
+      .json({ message: "User orders found", data: userWithOrders!.orders });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const addOrder = async (
+  req: Request<
+    {},
+    {},
+    { productId: string; status: "pending" | "delivered" | "cancelled" },
+    {}
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const checkData = await checkOrder({ status: req.body.status });
+    if (checkData !== true) {
+      errorGenerate("Invalid Inputs", 400, checkData);
+    }
+    const user = await User.findOne({ email: req.user }).exec();
+
+    if (!user) {
+      errorGenerate("User not found", 404);
+    }
+
+    const product = await Product.findOne({ _id: req.body.productId }).exec();
+
+    if (!product) {
+      errorGenerate("Product not found", 404);
+    }
+    const newOrder = await Order.create({
+      productId: product!.id,
+      userId: user!.id,
+      status: req.body.status,
+    });
+    user?.orders.push(newOrder as any);
+    await user?.save();
+
+    res.status(201).json({ message: "Order created", data: newOrder });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteOrder = async (
+  req: Request<{ oid: string }, {}, {}, {}>,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.oid)) {
+    errorGenerate("Invalid id", 400);
+  }
+  try {
+    const order = await Order.findById(req.params.oid);
+    const user = await User.findOne({ email: req.user });
+
+    if (!order) {
+      errorGenerate("Order not found", 404);
+    }
+
+    if (order?.userId.toString() !== user?._id.toString()) {
+      errorGenerate("You are not authorized to delete this order", 403);
+    }
+
+    await order!.remove();
+    await user?.updateOne({ $pull: { orders: req.params.oid } });
+    await user?.save();
+    res.status(200).json({
+      message: "Order deleted",
+      data: order,
+    });
+  } catch (err) {
+    return next(next(err));
+  }
+};
