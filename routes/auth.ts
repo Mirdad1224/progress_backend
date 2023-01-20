cff1a9358dcda3545e48fa59dea656b0eefac771
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-20 17:32:29 +0330

    add some comments for describing routes

--- routes/auth.ts
+++ routes/auth.ts
@@ -4,14 +4,19 @@ import loginLimiter from "../middlewares/loginLimiter";
 
 const router = Router();
 
+//Register a new user
 router.route("/register").post(loginLimiter, register);
 
+//Create or login user via social networks
 router.route("/social").post(social);
 
+//Login user
 router.route("/login").post(loginLimiter, login);
 
+//Get a new access token 
 router.route("/refresh").get(refresh);
 
+//Logout user
 router.route("/logout").post(logout);
 
 export default router;
--- routes/upload.ts
+++ routes/upload.ts
@@ -3,9 +3,16 @@ import {profileImage, productImage, productGallery, articleImage} from '../contr
 
 const router = Router();
 
+//Upload a profile image
 router.post("/profile", profileImage);
+
+//Upload the main product image
 router.post("/prodimg", productImage);
+
+//Upload a gallery for a specific product
 router.post("/prodgallery", productGallery);
+
+//Upload an article image
 router.post("/articleimg", articleImage);
 
 export default router
